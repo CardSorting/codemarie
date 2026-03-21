5a0dd1b093a51d646bb55988391c0f39313ccc48
CardSorting <willcruzdesigner@gmail.com>
2026-03-21 15:10:56 -0600

    new

--- debug_activate.js
+++ debug_activate.js
@@ -21,17 +21,35 @@ const mockVscode = {
 			dispose: () => {},
 		}),
 		onDidChangeWorkspaceFolders: () => ({ dispose: () => {} }),
+		fs: { stat: async () => ({}) },
+		registerTextDocumentContentProvider: () => ({ dispose: () => {} }),
 	},
-	window: { createOutputChannel: () => ({ appendLine: () => {}, append: () => {} }) },
+	window: {
+		createOutputChannel: () => ({ appendLine: () => {}, append: () => {} }),
+		createTextEditorDecorationType: () => ({}),
+		tabGroups: { all: [] },
+		registerWebviewViewProvider: () => ({ dispose: () => {} }),
+		registerUriHandler: () => ({ dispose: () => {} }),
+	},
+	languages: {
+		registerCodeActionsProvider: () => ({ dispose: () => {} }),
+	},
 	extensions: { getExtension: () => null },
 	env: { uriScheme: "vscode", uiKind: 1, machineId: "123" },
+	Uri: {
+		parse: (s) => ({ fsPath: s, toString: () => s }),
+		from: () => ({ fsPath: "" }),
+		file: (s) => ({ fsPath: s, toString: () => s }),
+	},
 	RelativePattern: class {},
 	Disposable: class {},
+	CodeActionKind: { QuickFix: 1, Refactor: 2, RefactorExtract: 3, RefactorRewrite: 4 },
+	CodeAction: class {},
 }
 
 try {
 	// Use a Function constructor to simulate a CommonJS environment
-	const wrapper = new Function("require", "module", "exports", "__filename", "__dirname", extCode)
+	const wrapper = new Function("require", "module", "exports", "__filename", "__dirname", "process", "console", extCode)
 	const module = { exports: {} }
 	const customRequire = (name) => {
 		if (name === "vscode") return mockVscode
@@ -42,7 +60,7 @@ try {
 		}
 	}
 
-	wrapper(customRequire, module, module.exports, extPath, path.dirname(extPath))
+	wrapper(customRequire, module, module.exports, extPath, path.dirname(extPath), process, console)
 	console.log("Exports:", Object.keys(module.exports))
 	if (module.exports.activate) {
 		console.log("Found activate, calling it...")
@@ -52,6 +70,9 @@ try {
 				extension: { id: "test" },
 				extensionUri: { fsPath: "" },
 				globalStorageUri: { fsPath: "" },
+				globalState: { get: () => {}, update: async () => {} },
+				workspaceState: { get: () => {}, update: async () => {} },
+				secrets: { get: async () => {}, store: async () => {}, onDidChange: () => ({ dispose: () => {} }) },
 			})
 			.then(() => console.log("Activate resolved"))
 			.catch((e) => console.error("Activate failed:", e))
